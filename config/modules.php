cc07f76723abd11c10a2d25ab3bd89430635a2af
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-09-18 10:49:29 +0700

    Add Postman configuration for API authentication settings

--- config/modules.php
+++ config/modules.php
@@ -6,4 +6,14 @@ return [
     'aliases' => [],
     'api_prefix' => 'api',
     'stubs_path' => base_path('stubs/modules'),
+
+    'postman' => [
+        'auth_middleware' => [
+            'auth',
+            'auth:*'
+        ],
+        'auth_type' => 'bearer',
+        'token_variable' => 'access_token',
+        'base_url_variable' => 'base_url'
+    ]
 ];
